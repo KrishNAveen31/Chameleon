17b540aa355cc743750ed4b4604f786f5524793e
KrishNAveen31 <89973374+KrishNAveen31@users.noreply.github.com>
2024-09-18 16:21:51 +0530

    Fixing errors

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Chameleon",
     platforms: [
-        .iOS(.v13)  // Ensure that iOS is defined here
+        .iOS(.v15)  // Ensure that iOS is defined here
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
@@ -18,7 +18,16 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Chameleon"),
+            name: "Chameleon",
+            dependencies: [],
+            path: "Sources/Chameleon",
+        publicHeadersPath: "include",
+            cSettings: [
+                            .headerSearchPath("Sources/Chameleon/include")
+                        ],
+            linkerSettings: [
+                    .linkedFramework("UIKit")
+                ]),
         .testTarget(
             name: "ChameleonTests",
             dependencies: ["Chameleon"]),
